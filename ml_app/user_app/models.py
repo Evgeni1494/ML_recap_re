6fc7dc7e4687cbe1a7c3822f4f54b48fc82259e8
Evgeni1494 <yoni1@hotmail.fr>
2023-07-21 15:51:34 +0200

    Revert "Merge pull request #3 from Evgeni1494/MR-45-Django-Backend"
    This reverts commit 5bc925c8a43315408a41173b2f75c109b725a665, reversing
    changes made to a651946dc2902b012d047adccd31fcacd785aef2.

--- ml_app/user_app/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    birth_date = models.DateField(auto_now=False, null=True)
-    reset_password_token = models.UUIDField(null=True, blank=True, unique=True)
